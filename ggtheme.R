0818e58a2498b3f99f53c03c27982fc1d3e0b8b2
juozas <juozas@PetronisMac2.local>
2016-06-23 08:32:40 -0400

    Added generic volcano plot

---
+++ ggtheme.R
@@ -0,0 +1,15 @@
+
+# This function can be augmented with parameters
+# that will adjust the theme
+
+getUnifiedGGTheme <- function() { 
+  require(ggplot2)
+  theme(
+    text=element_text(size=7, color="black"),
+    axis.line.x = element_line(color="black", size=0.25),
+    axis.line.y = element_line(color="black", size=0.25),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank())  
+}
---
+++ volcano.R
@@ -0,0 +1,26 @@
+source("colors.R")
+source("ggtheme.R")
+
+plotVolcano <- function(
+  coefficients, pvalues, 
+  nonSignificant=colors$grey, 
+  negative=colors$blue, 
+  positive=colors$red,
+  xAxisLabel="Coefficient",
+  yAxisLabel="-log10(P)",
+  pAdjustmentMethod="bonferroni")
+{
+  require(ggplot2)
+  require(data.table)
+  pd <- data.table(X=coefficients, Y=-log10(pvalues))
+  pd[, Fill := as.numeric(p.adjust(pvalues, pAdjustmentMethod) < 0.05)]
+  pd[Fill == TRUE & X < 0, Fill := Fill + 1]
+
+  ggplot(pd, aes(X, Y, 
+    color=as.factor(Fill))) + 
+    geom_point(size=.15, alpha=1) + 
+    xlab(xAxisLabel) + 
+    ylab(yAxisLabel) + 
+    scale_color_manual(values=c(nonSignificant, positive, negative), guide=FALSE) + 
+    getUnifiedGGTheme()
+}
