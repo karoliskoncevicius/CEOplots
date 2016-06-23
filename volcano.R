d174d77dcd5405b4bc46a88c60a2c7f3e759a419
Matthew Carlucci <matt.carlucci.96@gmail.com>
2016-06-23 10:27:22 -0400

    Force Symmetrical X limits

--- volcano.R
+++ volcano.R
@@ -8,16 +8,18 @@ plotVolcano <- function(
   positive=colors$red,
   xAxisLabel="Coefficient",
   yAxisLabel=expression(-log[10]*'(p-value)'),
-  pAdjustmentMethod="bonferroni")
+  pAdjustmentMethod="bonferroni",
+  isPercent=F)
 {
   require(ggplot2)
   require(data.table)
   pd <- data.table(X=coefficients, Y=-log10(pvalues))
   pd[, Fill := as.numeric(p.adjust(pvalues, pAdjustmentMethod) < 0.05)]
   pd[Fill == TRUE & X < 0, Fill := Fill + 1]
-
+  rng <- max(abs(pd$X))*1.05
+  
   ggplot(pd, aes(X, Y, 
-    color=as.factor(Fill))) + 
+                      color=as.factor(Fill))) + 
     geom_point(size=.15, alpha=1) + 
     geom_hline(yintercept=-log10(0.05/length(pvalues)),linetype=3)+
     xlab(xAxisLabel) + 
@@ -25,6 +27,8 @@ plotVolcano <- function(
     scale_color_manual(values=c(nonSignificant, positive, negative), guide=FALSE) + 
     geom_vline(xintercept=0)+
     getUnifiedGGTheme()+
-    theme(axis.line.y=element_blank())
+    theme(axis.line.y=element_blank())+
+    scale_x_continuous(limits = c(-rng,rng))
+ 
   
 }
