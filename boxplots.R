ee1f0f7049c3e9d820bb8b4e3b5952c993e99d39
KarolisKoncevicius <Karolis@Koncevicius.lt>
2016-07-04 17:53:24 +0300

    outline=FALSE

--- boxplots.R
+++ boxplots.R
@@ -10,7 +10,7 @@ plotBoxplots <- function(ints, lots, cols, ...) {
   colors <- rep(cols, table(lots))
 
   par(mar=c(2.5,3,2,1))
-  boxplot(ints, col=colors, cex=0.1, xaxt="n", outline=TRUE, las=1,
+  boxplot(ints, col=colors, cex=0.1, xaxt="n", outline=FALSE, las=1,
           boxlwd=0.25, whisklty=1, whisklwd=0.25, pch=19, cex=0.2, ...
           )
 
