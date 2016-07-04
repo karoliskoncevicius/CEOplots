f8905d156484fb0783b7f16ceaf3473a4e1fb2ef
Matthew Carlucci <matt.carlucci.96@gmail.com>
2016-07-04 17:01:33 -0400

    Nice percentage on xaxis

--- densities.R
+++ densities.R
@@ -4,8 +4,9 @@
 # cols - colors for each lot (length = number of unique lots)
 
 source("colors.R")
+require(scales)
 
-plotDensities <- function(ints, lots, cols,xlab="array intensity",leglab="Lot") {
+plotDensities <- function(ints, lots, cols,xlab="array intensity",leglab="Lot",ispct=F) {
   colors <- rep(cols, table(lots))
   dens   <- apply(ints, 2, density)
 
@@ -13,11 +14,12 @@ plotDensities <- function(ints, lots, cols,xlab="array intensity",leglab="Lot") 
   ylim=range(sapply(dens, "[[", "y"))
 
   par(mar=c(3,3,2,1))
-  plot(NA, xlim=xlim, ylim=ylim, las=1)
+  plot(NA, xlim=xlim, ylim=ylim, las=1, xaxt='n')
   mapply(points, dens, type="l", col=adjustcolor(colors,alpha.f = 0.3))
 
   mtext("density", 2, line=2)
   mtext(xlab, 1, line=2)
+  if(ispct){axis(side=1, at=c(0,0.25,0.5,0.75,1),labels = percent(c(0,0.25,0.5,0.75,1)))}
 
   legend("topright", legend=unique(lots), fill=cols, title=leglab)
 }
