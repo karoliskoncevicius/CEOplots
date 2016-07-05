36100d1210e844a07ced528cfd123a1c9eb2ad73
Matthew Carlucci <matt.carlucci.96@gmail.com>
2016-07-05 12:24:32 -0400

    Swithed to violin

--- outliers.R
+++ outliers.R
@@ -21,13 +21,30 @@ plotOutliers <- function(cors,set,isoutlier,setID,ztlabel){
                        "lab"=ztlabel)
   
   # ggplot(subset(plotdf,!lt3sd),aes(Set,vals,color=lt3sd,group=ID))+
-    ggplot(plotdf,aes(Set,vals,color=lt3sd,group=ID))+
-    geom_point(position=position_dodge(width=0.5),size=0.25)+
-    geom_text(data=subset(plotdf,lt3sd),aes(Set,vals,label=lab),
-              position = position_dodge(width=0.5),angle=270,size=7*5/14)+
-    scale_color_manual(values=c("TRUE"=colors$purple,"FALSE"=colors$green))+
+    # ggplot(plotdf,aes(Set,vals,color=lt3sd,group=ID))+
+    # geom_point(position=position_dodge(width=0.5),size=0.25)+
+    # geom_text(data=subset(plotdf,lt3sd),aes(Set,vals,label=lab),
+    #           position = position_dodge(width=0.5),angle=270,size=7*5/14)+
+    # scale_color_manual(values=c("TRUE"=colors$purple,"FALSE"=colors$green))+
+    # getUnifiedGGTheme()+
+    # xlab("")+ylab("")+guides(color=F)+
+    # theme(axis.text.x=element_text(angle=90))
+    
+  cols <- unlist(colors)[-7]
+  names(cols) <- NULL
+  
+  ggplot(plotdf,aes(Set,vals,fill=Set))+
+    geom_violin(width=1,color="grey",scale = "width",size=0.25)+
+    # geom_text(data=subset(plotdf,lt3sd),aes(Set,vals,label=lab,group=ID),
+    #           position = position_dodge(width=0.5),
+    #           angle=90,size=7*5/14,color="black")+
+    geom_point(data=subset(plotdf,lt3sd),aes(group=ID),position=position_dodge(width=0.5),size=0.5)+
+    scale_color_manual(values=cols)+
+    scale_fill_manual(values=cols)+
     getUnifiedGGTheme()+
-    xlab("")+ylab("")+guides(color=F)+
-    theme(axis.text.x=element_text(angle=90))
+    ylim(c(0.9,1))+
+    xlab("")+ylab("")+guides(color=F,fill=F)+
+    theme(axis.text.x=element_text(vjust=0.5,angle=90))
+    
   
 }
