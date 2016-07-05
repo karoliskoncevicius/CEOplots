3c7b487383b9a31affc80eba1f22f7ff1d2ccc8f
KarolisKoncevicius <Karolis@Koncevicius.lt>
2016-07-05 19:22:22 +0300

    changed densities to work on list input

--- densities.R
+++ densities.R
@@ -1,30 +1,34 @@
 # INPUT:
-# ints - matrix of intensities with samples in columns.
-# lots - lot/batch/plate information for each sample (length = # of samples)
-# cols - colors for each lot (length = number of unique lots)
+# ints - list of intensity vectors
+# labs - label for each sample (length = # of samples)
+# cols - colors for each labael (length = number of unique labels)
+# ...  - additional parameters passed to plot()
 
 source("colors.R")
-require(scales);require(plyr)
+require(scales)
+require(plyr)
 
-plotDensities <- function(ints, lots, cols,xlab="array intensity",leglab="Lot",ispct=F,lpos="topright") {
-  colors <- mapvalues(lots,from=levels(factor(lots)),to=cols)
-  dens   <- apply(ints, 2, density)
-  
-  xlim=range(sapply(dens, "[[", "x"))
-  ylim=range(sapply(dens, "[[", "y"))
-  
+plotDensities <- function(ints, labs, cols, xlab="array intensity",
+                          leglab="Lot", ispct=F, lpos="topright", ...
+                          ) {
+  colors <- mapvalues(labs, from=levels(factor(labs)), to=cols)
+  dens   <- lapply(ints, density)
+
+  xlim <- range(sapply(dens, "[[", "x"))
+  ylim <- range(sapply(dens, "[[", "y"))
+
   par(mar=c(3,3,2,1))
-  if(ispct){
-    plot(NA, xlim=xlim, ylim=ylim, las=1, xaxt='n')
+  if(ispct) {
+    plot(NA, xlim=xlim, ylim=ylim, las=1, xaxt='n', ...)
     axis(side=1, at=c(0,0.25,0.5,0.75,1),labels = percent(c(0,0.25,0.5,0.75,1)))
-  }else{
-    plot(NA, xlim=xlim, ylim=ylim, las=1)
+  } else {
+    plot(NA, xlim=xlim, ylim=ylim, las=1, ...)
   }
-  mapply(points, dens, type="l", col=adjustcolor(colors,alpha.f = 0.3))
-  
+  mapply(points, dens, type="l", col=adjustcolor(colors, alpha.f=0.3))
+
   mtext("density", 2, line=2)
   mtext(xlab, 1, line=2)
- 
-  legend(lpos, legend=unique(lots), fill=cols, title=leglab)
+
+  legend(lpos, legend=unique(labs), fill=cols, title=leglab)
 }
 
