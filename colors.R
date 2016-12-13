145a723e0932692246e5e086792e7786aafe7b5d
Matthew Carlucci <matt.carlucci.96@gmail.com>
2016-12-13 14:36:15 -0500

    added colkey for feature <-> color pairings

--- colors.R
+++ colors.R
@@ -6,3 +6,19 @@ colors$purple <- "#5B04B6"
 colors$yellow <- "#FFFF00"
 colors$green  <- "#00B692"
 colors$grey   <- "#D2D2D2"
+
+#color assignments
+colkey <- list()
+colkey$wake   <- colors$yellow
+colkey$sleep  <- colors$purple
+colkey$nonsig <- colors$grey
+colkey$sig    <- colors$red
+colkey$lung   <- colors$orange
+colkey$liver  <- colors$green
+colkey$hypo   <- colors$blue
+colkey$hyper  <- colors$red
+colkey$A9     <- colors$orange
+colkey$A15    <- colors$purple
+colkey$A25    <- colors$green
+colkey$macro  <- colors$red
+colkey$cntrl  <- colors$green
